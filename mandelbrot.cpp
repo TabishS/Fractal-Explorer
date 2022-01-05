a8b13c0a1a050355dc3700d69cd597d31747b6c4
TabishS <tshaikenedar@gmail.com>
2022-01-04 22:38:19 -0700

    Removed circular verification and circular scaling notes

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -43,9 +43,6 @@ cs225::PNG mandelbrot() {
       pixel.a = 1;
     }
 
-// Starfish: xShift = -0.2713397521875522, yShift = 0.20449150682899375
-// Minibrot: xShift = -0.7, yShift = 0.0
-
 //Targeting:
   if(Targeting) {
     for(int i = Width/2 - 1; i < Width/2 + 1; ++i)
@@ -68,15 +65,6 @@ cs225::PNG mandelbrot() {
       }
   }
 
-// /* Verifying Circular Scaling */
-// int r = 405;
-// for(int i = Width/2 - r; i <= Width/2 + r; ++i)
-//   for(int j = Height/2 - r; j <= Height/2 + r; ++j)
-//     if((Width/2 - i) * (Width/2 - i) + (Height/2 - j) * (Height/2 - j) <= r * r) {
-//       cs225::HSLAPixel & pixel = png.getPixel(i, j);
-//       pixel.h = 360;
-//     }
-
   return png;
 }
 
@@ -89,19 +77,7 @@ int main() {
 // For shifting, move old values and only calculate new values?
 // Or recalculate all
 
-/*
-      X = 2.47 * j/Width - 1.235;
-      Y = 2.24 * i/Height - 1.12;
- */
-
 /* Can you have MI not change anything other than the depth of generation? I.e. have all the 
    colors outside of the Brot stay the same? Perhaps rethink the way that I/MI is used for
    data?
- */
-
-/* The outermost circle of the Brot isn't circular because one axis is scaled to (-2, 0.47)
-   while the other is scaled to (-1.12, 1.12). Scaling both to either range would fix this
-   issue, but this might be incorrect. However, scaling both to the same range does seem to
-   make the subbrots look circular, whereas they are ovular with different scalings.
-   Additionally, the outer circle should be a circle to be correct
  */
