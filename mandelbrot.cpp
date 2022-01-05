6da6efcff527912fbd7fe46831cf4dec032fb4c0
TabishS <tshaikenedar@gmail.com>
2022-01-05 08:44:00 -0700

    Testing zoom scaling

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -7,18 +7,21 @@ cs225::PNG mandelbrot() {
   int Width = 640, Height = 480;
   cs225::PNG png(Width, Height);
  
-  bool Targeting = 0;
+  bool Targeting = 1;
+
+  double delta = 0.006125/2;
+  int PosI, NegI;
 
   double x, y, X, Y, I, temp;
   double MI = 128; // Find optimal scaling with Zoom if desired
-  double Zoom = 2; // Expressed as a scale of magnitude from the original default Zoom
+  double Zoom = 3; // Expressed as a scale of magnitude from the original default Zoom
   double ScaleWidth = 4.5; // The length/width of the Real-Imaginary plane that the set is calculated in
   double CenterxShift = -3/(4 * ScaleWidth);
   double xShift = CenterxShift, yShift = 0.0; // Expressed as a percentage of the width and height of the image
   // For a scale width of 4, the centering xShift is -3/(4 * 4) = -0.1875. For a scale width of 5, it is -3/(5 * 4) = -0.15
   // 4.5 is used as the scale width since it allows more to be on the screen while still being close to 4
   // The centering xShift is the xShift that results in the horizontal center of the screen
-  // being along thevertical axis where the main cardioid meets the biggest circle
+  // being along the vertical axis where the main cardioid meets the biggest circle
 
   int SmallerDim = Width < Height ? Width : Height; // Use for scaling to smaller dimension and maintaining circularity
   int XShift = SmallerDim == Width ? 0 : (Height - Width)/2;
@@ -41,11 +44,26 @@ cs225::PNG mandelbrot() {
       pixel.s = 1;
       pixel.l = 1 - I/MI;
       pixel.a = 1;
+      if(Y <= (0.25 + delta) && Y >= (0.25 - delta)) {
+        pixel.h = 360;
+        pixel.l -= 0.1;
+        PosI = i;
+      }
+      if(Y >= -(0.25 + delta) && Y <= -(0.25 - delta)) {
+        pixel.h = 360;
+        pixel.l -= 0.1;
+        NegI = i;
+      }
     }
+
+  std::cout << PosI - NegI << '\n';
+
+
+// Starfish: xShift = CenterxShift + 0.04173275077265841, yShift = 0.1428482553495056
 
 //Targeting:
   if(Targeting) {
-    for(int i = Width/2 - 1; i < Width/2 + 1; ++i)
+    for(int i = Width/2 - 1; i <= Width/2 + 1; ++i)
       for(int j = 0; j < Height; ++j) {
         cs225::HSLAPixel & pixel = png.getPixel(i, j);
         pixel.h = 360 - pixel.h;
@@ -53,9 +71,9 @@ cs225::PNG mandelbrot() {
         pixel.l = 1 - pixel.l;
         pixel.a = 1;
       }
-    for(int j = Height/2 - 1; j < Height/2 + 1; ++j)
+    for(int j = Height/2 - 1; j <= Height/2 + 1; ++j)
       for(int i = 0; i < Width; ++i) {
-        if(i >= Width/2 - 1 && i <= Width/2)
+        if(i >= Width/2 - 1 && i <= Width/2 + 1)
           continue;
         cs225::HSLAPixel & pixel = png.getPixel(i, j);
         pixel.h = 360 - pixel.h;
